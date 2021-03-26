1d7972c645bf45cb03cc690a1808d850d5d7b195
TaariqKhan <taariqkhs@gmail.com>
2021-03-26 15:35:01 +0200

    Testing first commit project

---
+++ src/main/java/za/ca/cput/testmaven3/testMavenTdd3.java
@@ -0,0 +1,22 @@
+package za.ca.cput.testmaven3;
+/**
+ *
+ * @author Taariq
+ */
+public class testMavenTdd3 
+{
+     public static void main(String[]args)
+   {
+       String a = new String ("taariq");
+       String b = new String ("taariq");
+       
+       if(a==b) //(a==b) returns the object(e.g which is diffrent in this case and returns false), (a.equals(b))checks object string use(e.g which is the same and returns true)
+       {
+            System.out.println(true);
+       }
+       else
+       {
+           System.out.println(false);
+       }
+   }
+}
