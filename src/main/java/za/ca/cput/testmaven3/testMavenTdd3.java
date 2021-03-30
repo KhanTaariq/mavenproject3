5aad81a2343e5f6ef7fed44bfd2b2343ddc499fa
Tumelo Mzaca <206006330@mycput.ac.za>
2021-03-30 12:52:32 +0200

    First commit:Changed the string name from Taariq to Tumelo

--- src/main/java/za/ca/cput/testmaven3/testMavenTdd3.java
+++ src/main/java/za/ca/cput/testmaven3/testMavenTdd3.java
@@ -7,8 +7,8 @@ public class testMavenTdd3
 {
      public static void main(String[]args)
    {
-       String a = new String ("taariq");
-       String b = new String ("taariq");
+       String a = new String ("tumelo"); //Changed the string name from Taariq to Tumelo
+       String b = new String ("tumelo");
        
        if(a==b) //(a==b) returns the object(e.g which is diffrent in this case and returns false), (a.equals(b))checks object string use(e.g which is the same and returns true)
        {
